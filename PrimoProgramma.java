a72805a7b6f41dd1cdcf7dda497e27bb458f469b
Massimo Di Litta <58837606+emmedielle@users.noreply.github.com>
2022-02-04 09:41:18 +0100

    Update PrimoProgramma.java

--- PrimoProgramma.java
+++ PrimoProgramma.java
@@ -6,23 +6,22 @@ public class PrimoProgramma {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Ciao!");  //invia allop schermo la stringa "Ciao!"
+		System.out.println("Ciao!");  //invia allo schermo la stringa "Ciao!"
 		System.out.println("Eseguo la somma di due numeri");
-		System.out.println("Inserisci due nuemri ineteri:");
+		System.out.println("Inserisci due numeri interi:");
 		
-		//dichiarazione di due variabili di tipo nuemrico intero
+		//dichiarazione di due variabili di tipo numerico intero
 		int n1, n2;
 		
-		//creazione dell'oggetto tastiera della classe Scanne
-		Scanner tastiera = new Scanner(System.in);   //predispone il programmamma affichè possa leggere l'input dalla tatstiera
+		//creazione dell'oggetto tastiera della classe Scanner
+		Scanner tastiera = new Scanner(System.in);   //predispone il programmamma affichÃ¨ possa leggere l'input dalla tatstiera
 		
-		n1=tastiera.nextInt();	//legge un numero inetro dalla tastiera
+		n1=tastiera.nextInt();	//legge un numero intero dalla tastiera
 		n2=tastiera.nextInt();
 		
 		int somma = n1+n2;
 		
-		System.out.println("La somma dei due numeri è " + somma);
+		System.out.println("La somma dei due numeri Ã¨ " + somma); 
+	} 
 	
-	}
-
 }
